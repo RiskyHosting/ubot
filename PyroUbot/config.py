035d7e4b692acd3d794b83fe00b794bd31be7dc6
RiskyHosting <riskydinataa@gmail.com>
2026-01-08 17:37:06 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -7,9 +7,9 @@ MAX_BOT = int(os.getenv("MAX_BOT", "200"))
 
 DEVS = list(map(int, os.getenv("DEVS", "7851958065").split()))
 
-API_ID = int(os.getenv("API_ID", "27216878"))
+API_ID = int(os.getenv("API_ID", "36320479"))
 
-API_HASH = os.getenv("API_HASH", "5d522cb56680802e92cb6301d59d7163")
+API_HASH = os.getenv("API_HASH", "d7eb20a7a335d13dfba7cc5b7720bea5")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7999962174:AAFmnloJ4Ol_JVpH2dYM_2BvpY_Tue6vKhU")
 
